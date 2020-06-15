7fb5957ebac216a9d68974201788cbe5c15221e8
soutaschool <55971123+soutaschool@users.noreply.github.com>
2020-06-15 22:51:31 +0900

    add-my-own-boirelaity

--- src/App.js
+++ src/App.js
@@ -1,29 +1,9 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          これでニキビのアプリを作ります。<br/>
-          妥協せずちゃんと作ります。<br/>
-          末長く見守ってやってください<br/>
-          大変恐縮ですが何卒よろしくお願いします <br/>
-
-          <br/>
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          作成者　佐々木颯汰
-        </a>
-      </header>
+      <h1>Hello, World</h1>
     </div>
   );
 }
