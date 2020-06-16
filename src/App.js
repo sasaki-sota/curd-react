90d235faaad21c7626b2629f3051d1386f03fb3f
soutaschool <55971123+soutaschool@users.noreply.github.com>
2020-06-16 11:00:20 +0900

    add-more-jsx

--- src/App.js
+++ src/App.js
@@ -1,17 +1,14 @@
+// JSXの時は必ずreactを書く
 import React, { Component } from "react";
 
-// class App extends Component{
-//   render() {
-//     return React.createElement(
-//         "div",
-//         null,
-//         "Hello world!!"
-//     );
-//   }
-// }
 class App extends Component {
   render() {
-    return <div>Hello, world</div>
+    return (
+        <React.Fragment>
+          <label htmlFor="bar">bar</label>
+          <input type="text" onClick={() => {console.log('I am clicked')}} />
+        </React.Fragment>
+    )
   }
 }
 
