7beadf62a6789cdc53b82cc521702714341fb174
soutaschool <55971123+soutaschool@users.noreply.github.com>
2020-06-16 10:21:42 +0900

    add-why-jsx

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello, World</h1>
-    </div>
-  );
+// class App extends Component{
+//   render() {
+//     return React.createElement(
+//         "div",
+//         null,
+//         "Hello world!!"
+//     );
+//   }
+// }
+class App extends Component {
+  render() {
+    return <div>Hello, world</div>
+  }
 }
 
-export default App;
+export  default App;
